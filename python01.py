23510fe5fb7648da346bfd8f33e52e2298459ed8
mguyon-alt <76257338+mguyon-alt@users.noreply.github.com>
2020-12-22 13:48:07 +0100

    cp sélectionnés

--- python01.py
+++ python01.py
@@ -13,10 +13,18 @@ for index, row in excel.iterrows():
     id=row[0]
     nomLab=row[1]
     cp= row[4]
-    if isinstance(cp, int) and (cp > 75000) :
+    if isinstance(cp, int) and ((cp > 75000) and (cp < 76000)) :
         data.append(row)
         nbAjout+=1
-
+    if isinstance(cp, int) and ((cp > 78000) and (cp < 79000)) :
+        data.append(row)
+        nbAjout+=1
+    if isinstance(cp, int) and ((cp > 92000) and (cp < 93000)) :
+        data.append(row)
+        nbAjout+=1
+    if isinstance(cp, int) and ((cp > 94999) and (cp < 96000)) :
+        data.append(row)
+        nbAjout+=1
 print("Ajouts : "+ str(nbAjout))
 
 #creation du dataframe pour le nouveau tableau avec nouvelles donnees
