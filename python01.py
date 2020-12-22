a8028c7c43695eb38e446eed7c0409b673e411d6
mguyon-alt <76257338+mguyon-alt@users.noreply.github.com>
2020-12-22 13:34:28 +0100

    ajouts de commentaires

--- python01.py
+++ python01.py
@@ -6,24 +6,10 @@ file = 'Agro.xlsx'
 # Load spreadsheet
 excel = pd.read_excel(file)
 
-# cp = excel["4"]
-# print(cp.head())
-
-
-#print(df1)
-# Load a sheet into a DataFrame by name: df1
-# df1 = xl.parse('Sheet1')
-
-#writer = pd.write_excel('new-example.xlsx', engine='xlsxwriter')
-
-
-# df1 = pd.DataFrame(ages.head(),
-#                    columns=['col 1', "Ages"])
-newExcel= pd.DataFrame([[1, 2], [3, 4]], columns=list('AB'))
 nbAjout=0
 data = []
+
 for index, row in excel.iterrows():
-    #colonne=newExcel.iloc[[0]]
     id=row[0]
     nomLab=row[1]
     cp= row[4]
@@ -32,5 +18,9 @@ for index, row in excel.iterrows():
         nbAjout+=1
 
 print("Ajouts : "+ str(nbAjout))
+
+#creation du dataframe pour le nouveau tableau avec nouvelles donnees
 newExcel = pd.DataFrame(data)
+
+# Creation du fichier excel
 newExcel.to_excel("resultats.xlsx")
