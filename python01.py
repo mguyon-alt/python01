1f905c79ccdf4a2d56a69b731193ca62fddb6bac
mguyon-alt <76257338+mguyon-alt@users.noreply.github.com>
2020-12-22 14:02:30 +0100

    supression colonne inutile

--- python01.py
+++ python01.py
@@ -25,10 +25,12 @@ for index, row in excel.iterrows():
     if isinstance(cp, int) and ((cp > 94999) and (cp < 96000)) :
         data.append(row)
         nbAjout+=1
+
 print("Ajouts : "+ str(nbAjout))
 
 #creation du dataframe pour le nouveau tableau avec nouvelles donnees
 newExcel = pd.DataFrame(data)
+del newExcel[0]
 
 # Creation du fichier excel
 newExcel.to_excel("resultats.xlsx")
